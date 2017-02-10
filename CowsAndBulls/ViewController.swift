a04867b70211b84bdf2e3f9f14df7db211dedbcf
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-09 19:40:43 -0500

    Viewcontroller now conforms to NSTableViewDelegate/Datasource with appropriate methods added

--- CowsAndBulls/ViewController.swift
+++ CowsAndBulls/ViewController.swift
@@ -8,11 +8,13 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     
     @IBOutlet var tableView: NSTableView!
-    
     @IBOutlet var textFieldGuess: NSTextField!
+    
+    var answer = ""
+    var guesses = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +31,51 @@ class ViewController: NSViewController {
     @IBAction func submitGuess(_ sender: Any) {
         
     }
+    
+    func result(for guess: String) -> String {
+        
+        return "Result"
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        
+        return guesses.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        guard let vw = tableView.make(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        
+        if tableColumn?.title == "Guess" {
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
